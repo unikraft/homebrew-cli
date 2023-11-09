523f296203ec1777a910bfa95e3ee759d09b8ee0
Cezar Craciunoiu <cezar.craciunoiu@gmail.com>
2023-11-09 18:01:16 +0200

    Fix dependencies format
    Signed-off-by: Cezar Craciunoiu <cezar.craciunoiu@gmail.com>

--- Formula/kraftkit.rb
+++ Formula/kraftkit.rb
@@ -5,20 +5,19 @@ class Kraftkit < Formula
   desc "Build and use highly customized and ultra-lightweight unikernels."
   homepage "https://kraftkit.sh"
   version "0.7.0"
-
-  depends on gnu-sed
-  depends on make
-  depends on coreutils
-  depends on m4
-  depends on gawk
-  depends on grep
-  depends on wget
-  depends on qemu
-  depends on socat
-  depends on x86_64-elf-binutils
-  depends on x86_64-elf-gcc
-  depends on aarch64-elf-binutils
-  depends on aarch64-elf-gcc
+  depends_on "gnu-sed"
+  depends_on "make"
+  depends_on "coreutils"
+  depends_on "m4"
+  depends_on "gawk"
+  depends_on "grep"
+  depends_on "wget"
+  depends_on "qemu"
+  depends_on "socat"
+  depends_on "x86_64-elf-binutils"
+  depends_on "x86_64-elf-gcc"
+  depends_on "aarch64-elf-binutils"
+  depends_on "aarch64-elf-gcc"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/unikraft/kraftkit/releases/download/v0.7.0/kraftkit_0.7.0_darwin_universal.zip"
